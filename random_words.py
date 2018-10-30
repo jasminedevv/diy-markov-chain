e7d33bd93f139dedf6545e0753028404b511a52f
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2018-10-30 14:05:51 -0700

    simplified random words

--- random_words.py
+++ random_words.py
@@ -1,29 +1,23 @@
 import random
+from sys import argv
 
-# reformat this to only need to open the file once
 def get_word(wordlist):
     '''
-    Returns a random word from the wordlist excluding contractions. 
+        Returns a random word from wordlist
     '''
-    word = random.choice(wordlist)
-    # prevents it from grabbing words with single quotes in them
-    while '\'' in word:
-        word = random.choice(words)
-    return word
-
+    return random.choice(wordlist)
+    
 def get_words(num, wordlist):
     '''
-        Takes an int.
-        Returns that number of words from the system dictionary.
+        Returns num number of words from the wordlist
     '''
-    words = []
+    words = list()
     for _ in range(num):
         word = get_word(wordlist)
         words.append(word)
     return words
 
 if __name__ == "__main__":
-    from sys import argv
     with open("/usr/share/dict/words") as words:
         words = words.read().splitlines()
         my_words = get_words(int( argv[1] ), words)
